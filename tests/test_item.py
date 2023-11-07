506bb89dc29376a9316fdb1e4448c9b91d0149f9
Stereo2025 <teneloks@gmail.com>
2023-11-07 09:44:56 +0300

    HW_1

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,28 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+
+@pytest.fixture
+def item():
+    return Item("Смартфон", 10000, 20)
+
+
+def test_calculate_total_price(item):
+    assert item.calculate_total_price() == 200000
+
+
+def test_apply_discount(item):
+    Item.pay_rate = 0.8
+    item.apply_discount()
+
+    assert item.price == 8000.0
+
+
+def test_all_items_list(item):
+    item2 = Item("Ноутбук", 20000, 5)
+
+    assert len(Item.all) == 2
+    assert item in Item.all
+    assert item2 in Item.all
+    Item.all.clear()
